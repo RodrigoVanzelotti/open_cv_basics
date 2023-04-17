abf9fb0f2da4b57424bcc1eb158a629a2f4db045
RodrigoVanzelotti <rodrigovanzelotti@gmail.com>
2023-04-17 16:22:39 -0300

    started openCV advanced

---
+++ 4_opencv_advanced/1color_spaces.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+'''
+O openCV trabalha de maneira padrão em BGR que é extremamente semelhante ao RGB, colorspace mais utilizado, a diferença é que:
+No RGB temos: (Red, Green, Blue) para a representação de cada pixel na tela
+No BGR invertemos as letras, logo temos: (Blue, Green, Red) para a representação de cada pixel na tela.
+
+ColorSpaces são maneiras diferentes de representar uma mesma imagem, em espaços de cor diferentes. GrayScale é um tipo de ColorSpace também
+Para representação de impressões utilizamos o CMYK (Ciano, Magenta, Amarelo, Preto), que representa a mistura dessas cores em um fundo branco,
+enquanto o RGB ou BGR, representa a combinação de Vermelho, Verde e Azul em um fundo preto.
+
+Essa aula é mais para apresentar os conceitos de colorspaces do que de fato descorrer sobre todos, mas os mais utilizados no mundo da informática são:
+RGB: praticamente padrão
+BGR: representado pelo OpenCV como padrão
+CMYK: representação para impressões em mundo real
+'''
+img = cv2.imread('assets/fotos/cat.jpg')
+
+# BGR to Grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Gray', gray)
+
+# BGR to HSV
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cv2.imshow('HSV', hsv)
+
+# BGR to L*a*b
+lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+cv2.imshow('LAB', lab)
+
+# BGR to RGB
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow('RGB', rgb)
+
+# HSV to BGR
+lab_bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+cv2.imshow('LAB --> BGR', lab_bgr)
+
+
+cv2.waitKey(0)
