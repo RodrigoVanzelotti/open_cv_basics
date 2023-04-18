b25d65169775ddf2d54859f0e1a3e710e748f766
RodrigoVanzelotti <rodrigovanzelotti@gmail.com>
2023-04-18 10:38:01 -0300

    complementing masking.py

--- 4_opencv_advanced/5masking.py
+++ 4_opencv_advanced/5masking.py
@@ -8,21 +8,30 @@ O mascaramento é uma técnica comum para extrair a região de interesse (ROI - 
 No openCV, é possível construir uma forma de máscara arbitrária usando a função de desenho e as Bitwise Operations.
 '''
 
+'''
+Nas aulas de Bitwise Operations aprendemos que podiamos utilizar 2 imagens e manipular o resultado a partir das suas intersecções.
+O que não aprendemos é que podemos utilizar essas operações com 2 imagens e uma máscara para o recorte de imagens coloridas.
+Então nessa aula iremos criar uma região de interesse (a partir de uma bitwise operation entre duas formas simples) e na sequencia recortar uma imagem com essa região.
+'''
+
 img = cv2.imread('assets/fotos/cats.jpg')
-cv2.imshow('AsimoCats', img)
+# cv2.imshow('AsimoCats', img)
 
 # tela branca (que na verdade é preta)
 blank = np.zeros(img.shape[:2], dtype='uint8')
 # cv2.imshow('Blank Image', blank)
 
-# criando frmas para trabalhar acerca
+# criando formas para trabalhar acerca
 circle = cv2.circle(blank.copy(), (img.shape[1]//2 + 45,img.shape[0]//2), 100, 255, -1)
 rectangle = cv2.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
+cv2.imshow('Circulo', circle)
+cv2.imshow('Retangulo', rectangle)
 
-recorte = cv2.bitwise_and(circle,rectangle)
+
+recorte = cv2.bitwise_and(circle, rectangle)
 cv2.imshow('Nosso recorte para a operacao', recorte)
 
-masked = cv2.bitwise_and(img,img,mask=recorte)
+masked = cv2.bitwise_and(img, img, mask=recorte)
 cv2.imshow('Imagem mascarada', masked)
 
 '''
@@ -31,21 +40,4 @@ desenhar os contornos com as cores originais da imagem, só pra praticar
 Resolver na aula seguinte!
 '''
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (5,5), cv2.BORDER_DEFAULT)
-canny = cv2.Canny(blur, 125, 175)
-cv2.imshow('Canny', canny)
-
-
-contours, hierarchies = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-# contours, hierarchies = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-print(f'{len(contours)} contornos encontrados!!')
-
-yo = cv2.drawContours(blank, contours, -1, (0,0,255), 1)
-cv2.imshow('Contornos desenhados', yo)
-
-masked2 = cv2.bitwise_and(img,img,mask=recorte)
-cv2.imshow('Imagem mascarada', masked2)
-
 cv2.waitKey(0)
-
